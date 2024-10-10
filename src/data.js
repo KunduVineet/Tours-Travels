6e4832e3177d4225b2485fe7231fc02c74dfefc0
VineetKundu <kunduvineet6@gmail.com>
2024-10-10 21:05:28 +0530

    Tours and Travels

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const data = [
+    {
+      id: 1,
+      name: "Jaipur, Rajasthan",
+      info: "Explore the Pink City, famous for its grand palaces, forts, and vibrant culture.",
+      image: "https://img.freepik.com/free-photo/hawa-mahal-palace-jaipur-india_53876-31311.jpg?t=st=1728568709~exp=1728572309~hmac=4509ce25c6ed17cca57c88b68225c09603daf93e3369e4dece000a42d57c49d3&w=996",
+      price: 1200,
+    },
+    {
+      id: 2,
+      name: "Varanasi, Uttar Pradesh",
+      info: "Experience the spiritual capital of India, known for its ancient temples and the sacred Ganges River.",
+      image: "https://img.freepik.com/premium-photo/sacred-ganges-river-varanasi-with-ancient-ghats_996201-69778.jpg?w=1060",
+      price: 1100,
+    },
+    {
+      id: 3,
+      name: "Goa",
+      info: "Relax on the pristine beaches of Goa, famous for its nightlife, water sports, and colonial architecture.",
+      image: "https://img.freepik.com/free-photo/beautiful-tropical-beach-sea-ocean-with-coconut-palm-tree-sunrise-time_74190-7454.jpg?t=st=1728573514~exp=1728577114~hmac=50718213e5545348508f0e7f9574b366f9fef3b8eef1f138f443f730b2a0c43b&w=826",
+      price: 1400,
+    },
+    {
+      id: 4,
+      name: "Leh, Ladakh",
+      info: "Discover the beauty of the high-altitude desert, surrounded by stunning mountains and serene monasteries.",
+      image: "https://img.freepik.com/free-photo/indian-travel-destination-lake-mountain-landscape_53876-16458.jpg?t=st=1728573638~exp=1728577238~hmac=24e95ae321c4deb1da43a8fe11c009e14585b3047c68dca7d66b3a7ecd644ff1&w=996",
+    },
+    {
+      id: 5,
+      name: "Udaipur, Rajasthan",
+      info: "Visit the City of Lakes, known for its majestic palaces, lakes, and rich history.",
+      image: "https://img.freepik.com/free-photo/beautiful-shot-udaipur-from-window-city-palace_181624-44764.jpg?t=st=1728573712~exp=1728577312~hmac=a04dea45029c9c9d08b138a0c43804a2a872098adbdacd257ad160079ddc02b8&w=996",
+      price: 1300,
+    },
+    {
+      id: 6,
+      name: "Mysore, Karnataka",
+      info: "Explore the royal heritage of Mysore, home to magnificent palaces and vibrant festivals.",
+      image: "https://img.freepik.com/premium-photo/mysore-pak-hindu_1279582-4113.jpg?w=996",
+      price: 1000,
+    },
+    {
+      id: 7,
+      name: "Kochi, Kerala",
+      info: "Visit the Queen of the Arabian Sea, famous for its backwaters, beaches, and colonial history.",
+      image: "https://img.freepik.com/free-photo/sunrise-vasco-da-gama-bridge-lisbon-portugal_268835-1338.jpg?t=st=1728573845~exp=1728577445~hmac=438d2a7655cb319922f286f7a46305d4789dc6d913c00a506add8b862077f2b7&w=996",
+      price: 1350,
+    },
+    {
+      id: 8,
+      name: "Darjeeling, West Bengal",
+      info: "Enjoy the scenic beauty of the Himalayan town, known for its tea plantations and stunning mountain views.",
+      image: "https://img.freepik.com/free-photo/old-fishing-boat-river-with-breathtaking-view-sunset_181624-14111.jpg?t=st=1728573995~exp=1728577595~hmac=c220b873a0d2b0000669cf736bfc201d89a76f711a3c10488d8414da2622911b&w=826",
+      price: 1500,
+    }
+  ];
+  
+  export default data;
+  
